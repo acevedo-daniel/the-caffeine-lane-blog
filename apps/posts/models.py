c3efb50461e5fb23fd33b15e6e0f499274b95922
Acevedo <mrdanielacevedo@gmail.com>
2025-07-29 23:54:36 -0300

    update ui home

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
