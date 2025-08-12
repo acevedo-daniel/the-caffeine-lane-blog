efa3b41261fd0139b29463cbdd4bcc87400e7b88
Acevedo <mrdanielacevedo@gmail.com>
2025-08-12 05:19:04 -0300

    lazy updates

--- apps/posts/apps.py
+++ apps/posts/apps.py
@@ -1,13 +1,9 @@
-
 from django.apps import AppConfig
 
+
 class PostsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.posts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.posts"
 
     def ready(self):
-        from .models import Category, Post, Comment
-        # Si querés conectar señales, lo hacés acá
-
-
-    # ...existing code...
+        from .models import Category, Comment, Post
