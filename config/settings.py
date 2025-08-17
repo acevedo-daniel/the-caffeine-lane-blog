4369f37de4423c28b8691338f4fac903ec77df94
Acevedo <mrdanielacevedo@gmail.com>
2025-08-17 18:52:23 -0300

    postgresql update

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Load environment variables from .env file at the very top
 
 
@@ -87,10 +89,7 @@ TEMPLATES = [
 # ======================================================================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # ======================================================================
